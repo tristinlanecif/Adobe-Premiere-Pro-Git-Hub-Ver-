68bd444cfd904e4f263116a8de72af4775dc6950
tristinlanecif <142951423+tristinlanecif@users.noreply.github.com>
2023-12-18 21:28:53 -0600

    Create app.js

---
+++ resources/app/wrapper/app.js
@@ -0,0 +1,6 @@
+<app/>
+  <html>
+    <backgroundcolor>id="666666"<backgroundcolor/>
+    <button>h="21.4"; y="118.6"; w="51.0"; x="10.1"; colorid="333333"; text="Make A Video"<button/>
+      <page>name="Video Maker"; backgroundcolor="666666"<page/>
+    <button>h="21.4"; y="148.8"; w="51.0"; x="10.1"; colorid="333333"; text="Video List"<button/>
